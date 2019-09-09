5df2696583a106ed5ee7afd2356d08b82d738b17
Nick Meleshchenko <nick.meleshchenko@gmail.com>
2019-09-09 20:03:03 +0300

    dsaddasd

--- dbconfig/database.js
+++ dbconfig/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/e-shop'
-    database: 'mongodb+srv://Kostya:kikines88@e-shop-elmlx.mongodb.net/shop?retryWrites=true&w=majority'
+    database: 'mongodb+srv://melnick:melnick123@e-shop-elmlx.mongodb.net/shop?retryWrites=true&w=majority'
 }
