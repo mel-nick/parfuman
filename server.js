d401d899cb73673ad1426736549accd718eeec52
Nick Meleshchenko <nick.meleshchenko@gmail.com>
2019-09-10 14:23:07 +0300

    trying

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 const path = require('path');
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // parse application/json
