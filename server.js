fc5ccc9e82a61cdeba50995460d18f74563447a5
Nick Meleshchenko <nick.meleshchenko@gmail.com>
2019-09-10 15:05:34 +0300

    dfsdlkfls;dv

--- server.js
+++ server.js
@@ -13,22 +13,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/', require('./api'));
 
-// Serve static files from the React app
-
 //mongoose connect
 mongoose.connect(MONGODB_URI || config.database, { useNewUrlParser: true, useFindAndModify: false} );
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/build/index.html'));
-});
 
-if (process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client','build','index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
   });
-}
 
 // Note model
 const User = require('./dbmodels/user');
