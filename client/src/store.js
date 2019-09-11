f8927be344e5ea928cc0d75168074bc4f3998cec
Nick Meleshchenko <nick.meleshchenko@gmail.com>
2019-09-11 14:52:27 +0300

    remove access to devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,18 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const inititalState = {};
 
+// const store = createStore(
+//         rootReducer,
+//         inititalState,
+//         compose(applyMiddleware(thunk),
+//                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
 const store = createStore(
-        rootReducer, 
-        inititalState, 
-        compose(applyMiddleware(thunk), 
-                window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+        rootReducer,
+        inititalState,
+        compose(applyMiddleware(thunk)));
 
 export default store;
