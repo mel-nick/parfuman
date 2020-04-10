79b58080a6811deef2915991636aefc8e3a6c96a
mel-nick <48454124+mel-nick@users.noreply.github.com>
2020-04-10 23:36:59 +0300

    Delete database.js

--- dbconfig/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    // database: 'mongodb://localhost:27017/e-shop'
-    database: 'mongodb+srv://melnick:melnick123@e-shop-elmlx.mongodb.net/shop?retryWrites=true&w=majority'
-}
